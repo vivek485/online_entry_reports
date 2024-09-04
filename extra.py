7152a862d1031422c0957f1f5960cc256696af20
vivek485 <155722813+vivek485@users.noreply.github.com>
2024-09-04 17:13:31 +0530

    Add files via upload

---
+++ extra.py
@@ -0,0 +1,52 @@
+camp = ['YEAR', 'BLOCK', 'MONTH','NAME_OF_CAMP','TOTAL_CAMPS','DATES_SEPERATED_BY_COMMA','PLACE','MALE','FEMALE','CHILD','TOTAL']
+
+geptrlt = ['YEAR', 'BLOCK', 'MONTH', 'AHC', 'NEW_PATIENTS', 'OLD_PATIENTS', 'TOTAL_PATIENTS',
+         'MALE_PATIENTS', 'FEMALE_PATIENTS', 'CHILD_PATIENTS', 'TOTAL_PATIENT', 'NADI', 'PAACHAN', 'RAKT', 'SWASHAN',
+         'MUTRA', 'TWAK', 'EAREYE', 'JWAR', 'OTHER', 'TOTAL']
+ptrlt = ['YEAR', 'BLOCK', 'MONTH', 'AHC', 'NEW_PATIENTS', 'OLD_PATIENTS', 'TOTAL_PATIENTS',
+         'MALE_PATIENTS', 'FEMALE_PATIENTS', 'CHILD_PATIENTS', 'TOTAL_PATIENT', 'NADI', 'PAACHAN', 'RAKT', 'SWASHAN',
+         'MUTRA', 'TWAK', 'EAREYE', 'JWAR', 'OTHER', 'TOTAL']
+ipd_plt = ['YEAR', 'BLOCK', 'MONTH', 'AHC', 'NEW_PATIENTS', 'OLD_PATIENTS', 'TOTAL_PATIENTS',
+         'MALE_PATIENTS', 'FEMALE_PATIENTS', 'CHILD_PATIENTS', 'TOTAL_PATIENT', 'ger_NEW_PATIENTS', 'ger_OLD_PATIENTS', 'ger_TOTAL_PATIENTS',
+         'ger_MALE_PATIENTS', 'ger_FEMALE_PATIENTS', 'ger_CHILD_PATIENTS', 'ger_TOTAL_PATIENT',]
+
+adharcol = ['YEAR', 'BLOCK', 'MONTH', 'AHC', 'Total_patients_attended',
+            'Total_MALE_patients', 'Total_FEMALE_patients',
+            'Total_Aadhar_seeded_beneficiaries', 'Total_beneficiaries_having_Mob_No', 'DATE_OF_SESSION',
+            'TOTAL_NO_OF_SESSION', 'NO_OF_MALE_BENEFICIARY', 'NO_OF_FEMALE_BENEFICIARY', 'NO_OF_CHILD_BENEFICIARY',
+            'TOTAL_YOGA_BENEFICIARY_IN_MONTH']
+bmwcol = ['YEAR', 'BLOCK', 'MONTH', 'AHC', 'YELLOW_CATEGORY_IN_GRMS', 'RED_CATEGORY_IN_GRMS',
+          'WHITE_CATEGORY_IN_GRMS', 'BLUE_CATEGORY_IN_GRMS', 'TOTAL_IN_GRMS', 'LIQUID_WASTE_GENERATED']
+sapcol = ['YEAR', 'BLOCK', 'MONTH', 'AHC', 'DATE_OF_ACTIVITY', 'NAME_OF_SCHOOL', 'TYPE_OF_SCHOOL',
+          'SUBJECT_COVERED', 'TOTAL_BENEFICIARIES']
+tbcol = ['YEAR',	'BLOCK',	'MONTH',	'AHC',	'Total_monthly_OPD_A',	'70percent_Adult_OPD_B',	'No_of_TB_suspects_for_sputum',	'Referral_rate_D',	'TB_suspects_found_positive',	'Name_of_DMCs_where_patient_sent',	'TB_patients_on_DOTS_in_AHC',	'Patients_in_intensive_phase',	'Patients_in_continuation_phase',	'TB_patient_died',	'TB_patients_defaulted',	'sputum_cups_available_first_day',	'sputum_cups_available_last_day',	'sputum_cups_required',	'referral_slips_available_first_day',	'referral_slips_available_last_day',	'referral_slips_required']
+pkcol = ['YEAR', 'BLOCK', 'MONTH', 'AHC', 'OPD_NEW', 'OPD_OLD', 'OPD_MALE', 'OPD_FEMALE', 'OPD_CHILD',
+         'OPD_TOTAL', 'IPD_NEW', 'IPD_OLD', 'IPD_MALE', 'IPD_FEMALE', 'IPD_CHILD', 'IPD_TOTAL']
+kscol = ['YEAR', 'BLOCK', 'MONTH', 'AHC', 'NEW_OPD', 'OLD_OPD', 'MALE_OPD', 'FEMALE_OPD', 'CHILD_OPD',
+         'TOTAL_OPD', 'NEW_IPD', 'OLD_IPD', 'MALE_IPD', 'FEMALE_IPD', 'CHILD_IPD', 'TOTAL_IPD']
+poshancol = ['YEAR', 'BLOCK', 'MONTH', 'AHC', 'MALE', 'FEMALE', 'CHILD', 'TOTAL']
+ayuhimcol = ['YEAR', 'BLOCK', 'MONTH', 'AHC', 'AYUSHMAN_MALE', 'AYUSHMAN_FEMALE', 'AYUSHMAN_CHILD',
+             'AYUSHMAN_TOTAL', 'HIMCARE_MALE', 'HIMCARE_FEMALE', 'HIMCARE_CHILD', 'HIMCARE_TOTAL']
+anucol = ['YEAR', 'BLOCK', 'MONTH', 'AHC', 'MARM', 'JALOKA', 'RAKTMOKSHAN', 'ALABU', 'MRITIKA', 'CUPPING',
+          'AGNIKARMA', 'KSHARKARMA']
+
+
+
+
+
+
+
+#collection id
+pt = '66d68c2b0012ed468cd0'
+gpt = '66d68c3e003756913187'
+pt_ipd = '66d68cd0001e75d60d91'
+camps = '66d68cc7003614925df6'
+anu ='66d68cb900382fdfa5df'
+ayhim = '66d68ca700129fbcd779'
+pos = '66d68c9e002b9ea6db29'
+ks ='66d68c92002558a3552b'
+pk ='66d68c7b0039df93ae22'
+tb ='66d68c720030164ba886'
+sap = '66d68c64000463f8342a'
+bm = '66d68c5d00312ca21161'
+adhar ='66d7f4a4000d1f1837e9'
